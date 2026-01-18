5469c7d63d78ba24dc68dfd41f714d7d3999492c
DragonHunter274 <schurgel@gmail.com>
2026-01-18 15:24:26 +0100

    remove haskell stuff

--- hosts/hydra/configuration.nix
+++ hosts/hydra/configuration.nix
@@ -16,7 +16,7 @@ in
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
     ./hardware-configuration.nix
     inputs.sops-nix.nixosModules.sops
-    inputs.hydra-tools.nixosModules.hydra-github-bridge
+    # inputs.hydra-tools.nixosModules.hydra-github-bridge  # disabled - pulls in haskellNix/cabal
   ];
 
   networking.hostName = "hydra";
@@ -183,14 +183,15 @@ in
   };
 
   # Hydra GitHub Bridge - reports build status to GitHub
-  services.hydra-github-bridge.default = {
-    enable = false;
-    ghAppId = 2507762; # TODO: Replace with your actual GitHub App ID number
-    ghAppKeyFile = config.sops.secrets.github-app-key.path;
-    ghUserAgent = "hydra-github-bridge/1.0 (hydra.dh274.com)";
-    hydraHost = "https://hydra.dh274.com";
-    hydraDb = ""; # Use Unix socket connection
-  };
+  # Disabled - module pulls in haskellNix/cabal dependencies
+  # services.hydra-github-bridge.default = {
+  #   enable = false;
+  #   ghAppId = 2507762;
+  #   ghAppKeyFile = config.sops.secrets.github-app-key.path;
+  #   ghUserAgent = "hydra-github-bridge/1.0 (hydra.dh274.com)";
+  #   hydraHost = "https://hydra.dh274.com";
+  #   hydraDb = "";
+  # };
 
   # SOPS configuration
   sops = {
