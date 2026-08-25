d7b10e6b3db12ebf6283d49dda8f1f38ff29b26c
DragonHunter274 <schurgel@gmail.com>
2026-08-25 13:48:26 +0200

    fix gtk theme

--- hosts/thinkpad-simon/home.nix
+++ hosts/thinkpad-simon/home.nix
@@ -34,8 +34,8 @@
     enable = true;
 
     theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
+      package = pkgs.orchis-theme.override { tweaks = [ "black" ]; };
+      name = "Orchis-Grey-Dark";
     };
 
     iconTheme = {
